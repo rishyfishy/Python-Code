3e07f9c4c9dfbd2613345901b5389abd87a2ac38
Rishabh Garikiparithi <rishabh.garikiparithi@mail.utoronto.ca>
2021-02-17 23:07:03 -0500

    Fixed Gauss Jordan

--- gaussJordan.py
+++ gaussJordan.py
@@ -1,42 +1,41 @@
 
 def naiveGaussJordan(matrix: list, vector: list):
     n = len(matrix)
+    newMat = matrix.copy()
     # Forward Propogation
-    for k in range(n-1):
+    for k in range(n-1):    #every diagonal element except last
 
-        for i in range(k+1, n):
-            factor = matrix[i][k]/matrix[k][k]
+        for i in range(k+1, n):     #every row below the k'th diagonal element
+            factor = newMat[i][k]/newMat[k][k]
+            for j in range(k, n):
 
-            for j in range(k+1, n):
-                
-                matrix[i][j] -= factor*matrix[k][j]
+                newMat[i][j] -= factor*newMat[k][j]
 
             vector[i] -= factor*vector[k]
-            print(matrix)
+            print(newMat)
     # Back-substitution
     x = n*[0]
-    x[n-1] = vector[n-1]/matrix[n-1][n-1]
+    # x[n-1] = vector[n-1]/newMat[n-1][n-1]
 
-    for i in reversed(range(1, n)):
+    for i in reversed(range(n)):
         sum = vector[i]
         for j in range(i+1, n):
-            sum -= matrix[i][j]*x[j]
-        x[i] = sum/matrix[i][j]
+            sum -= newMat[i][j]*x[j]
+        x[i] = sum/newMat[i][i]
 
     return x
 
 # %%
-def partialPivoting(matrix:list,vector:list)->list:
-    n=len(vector)
-    x=n*[0]
 
- 
 
+def partialPivoting(matrix: list, vector: list) -> list:
+    n = len(vector)
+    x = n*[0]
 
     for k in range(1, n):
-    # Swap the first row and the row with max first element
+        # Swap the first row and the row with max first element
 
-       #zero out first column
+       # zero out first column
         for i in range(k+1, n+1):
             factor = matrix[i, k]/matrix[k, k]
 
@@ -45,9 +44,10 @@ def partialPivoting(matrix:list,vector:list)->list:
 
             vector[i] -= factor*vector[k]
 
-a=[[1,2,3],[1,4,9],[1,8,27]]
-b=[0,0,-6]
-answer = naiveGaussJordan(a,b)
+
+a = [[1, 2, 3], [1, 4, 9], [1, 8, 27]]
+b = [0, 0, -6]
+answer = naiveGaussJordan(a, b)
 print(answer)
 
 # %%
