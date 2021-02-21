b1d9af993c3bd86b879d980169faeff76c31ade5
Rishabh Garikiparithi <rishabh.garikiparithi@mail.utoronto.ca>
2021-02-21 00:19:48 -0500

    thomas

--- thomasAlgo.py
+++ thomasAlgo.py
@@ -1,16 +1,34 @@
 def Thomas(matrix, vector):
+    #forward prop
     for i in range(len(vector)-1):
-        factor = matrix[i][i]
+        matrix[i][i+1] /= matrix[i][i]
+        vector[i] /= matrix[i][i]
         matrix[i][i] = 1
-        matrix[i][i+1] /= factor
-        vector[i] /= factor
 
-        matrix[i+1][i+1] -= matrix[i][i+1]
-        vector[i] -= vector[i]
+        matrix[i+1][i+1] -= matrix[i+1][i]*matrix[i][i+1]
+        vector[i+1]-=matrix[i+1][i]*vector[i]
+        matrix[i+1][i]=0
 
-    i = len(vector)-1
-    factor = matrix[i][i]
+    i=len(vector)-1
+    vector[i] /= matrix[i][i]
     matrix[i][i] = 1
-    matrix[i][i+1] /= factor
-    vector[i] /= factor
-    return matrix, vector
+
+    #backprop
+
+    for i in reversed(range(len(vector)-1)):
+        vector[i]-=matrix[i][i+1]*vector[i+1]
+        matrix[i][i+1]=0
+
+
+
+
+
+
+matrix=[[2,-1,0,0,0],[-1,2,-1,0,0],[0,-1,2,-1,0],[0,0,-1,2,-1],[0,0,0,-1,2]]
+vector=[1,1,1,1,1]
+
+Thomas(matrix,vector)
+for i in matrix:
+    print (i)
+print('\n')
+print([round(x,5) for x in vector])
