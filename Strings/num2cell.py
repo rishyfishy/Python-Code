b551eed5fa344a8fe7472e6819fd98a749b61f5b
Rishabh Garikiparithi <rishabh.garikiparithi@mail.utoronto.ca>
2021-04-21 12:48:57 -0400

    threetwo

--- Strings/num2cell.py
+++ Strings/num2cell.py
@@ -4,9 +4,13 @@ def num2cell(num:int)->str:
     elif num<27:
         return chr(num+64)
     else:
-        return num2cell(num//27)+num2cell((num+1)%27)
+        a=(num-1)//26
+        return num2cell(a)+num2cell(num-26*a)
 
-print(num2cell(27))
-print(num2cell(27*27))
-print(num2cell(27**3))
-print(num2cell(27**4))
+# print(num2cell(27+26))
+# print(num2cell(51))
+# print(num2cell(52))
+# print(num2cell(676))
+# print(num2cell(702))
+# print(num2cell(704))
+# print(num2cell(456976))
